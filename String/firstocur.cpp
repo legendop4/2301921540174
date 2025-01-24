1261cd143bcc2146ccbdcb6d22ab15524309aec5
Shivam Sharma <128924595+legendop4@users.noreply.github.com>
2025-01-25 00:09:26 +0530

    Create firstocur.cpp

---
+++ String/firstocur.cpp
@@ -0,0 +1,11 @@
+//LEETCODE PROBLEM  Find the Index of the First Occurrence in a String
+// LINK : https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+// submission detail : https://leetcode.com/submissions/detail/1519188234/
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+        return haystack.find(needle);
+    }
+};

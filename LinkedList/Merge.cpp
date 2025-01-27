b9c4f29009fdf84cb7672a9f269e96ea58ed3519
Shivam Sharma <128924595+legendop4@users.noreply.github.com>
2025-01-28 01:11:56 +0530

    Update Merge.cpp

--- LinkedList/Merge.cpp
+++ LinkedList/Merge.cpp
@@ -3,3 +3,36 @@
 // submission detail : 
 
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy(0);
+        ListNode* prev = &dummy;
+        ListNode* fast = list1;
+        ListNode* slow = list2;
+        
+        while(fast != NULL && slow != NULL){
+            if(fast-> val < slow->val ){
+                prev-> next = fast;
+                fast = fast->next;
+            }
+            else {
+                prev-> next = slow;
+                slow = slow-> next;
+            }
+            prev = prev->next;
+        }
+        prev -> next = (fast!= NULL)?fast:slow;
+        return dummy.next;
+    }
+};

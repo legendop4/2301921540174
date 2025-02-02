d5eaea7339cb1fcbfe3b3c89c5e7edfcb80e25af
Shivam Sharma <128924595+legendop4@users.noreply.github.com>
2025-02-02 14:47:54 +0530

    Create slidingwindow.cpp

---
+++ Queue/slidingwindow.cpp
@@ -0,0 +1,35 @@
+//LEETCODE PROBLEM  SLiding Window Maximum
+// LINK : https://leetcode.com/problems/sliding-window-maximum/
+// submission detail : 
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> q;
+
+        for (int i = 0; i < k; i++) {
+            while(!q.empty() && nums[q.back()] < nums[i]) {
+                q.pop_back();
+            }
+            q.push_back(i);
+        }
+
+        ans.push_back(nums[q.front()]);
+
+        for (int i = k; i < nums.size(); i++) {
+            // removal
+            if (!q.empty() && i - q.front() >= k) {
+                q.pop_front();
+            }
+            // insertion
+            while(!q.empty() && nums[q.back()] < nums[i]) {
+                q.pop_back();
+            }
+            q.push_back(i);
+            // store
+            ans.push_back(nums[q.front()]);
+        }
+        return ans;
+    }
+};

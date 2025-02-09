b36d839517511e9d32556572042d90f1c16f6673
Shivam Sharma <128924595+legendop4@users.noreply.github.com>
2025-02-09 22:10:51 +0530

    Create row.cpp

---
+++ revision/easy/row.cpp
@@ -0,0 +1,29 @@
+// Problem Name : Search in a row wise sorted matrix
+// Link : https://www.geeksforgeeks.org/problems/search-in-a-row-wise-sorted-matrix/1
+class Solution {
+  public:
+    // Function to search a given number in row-column sorted matrix.
+      bool search(vector<int>&arr, int x){
+        int lo=0, hi=arr.size()-1;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(arr[mid]==x) return true;
+            else if(arr[mid]<x) lo=mid+1;
+            else hi=mid-1;
+        }
+        return false;
+    }
+  
+    // Function to search a given number in row-column sorted matrix.
+    bool searchRowMatrix(vector<vector<int>> &mat, int x) {
+        // code here
+        int n=mat.size(), m=mat[0].size();
+        for(int i=0;i<n;i++){
+            if(mat[i][0]<=x && x<=mat[i][m-1]){
+                if(search(mat[i], x)) return true;
+            }
+        }
+        return false;
+        
+    }
+};
